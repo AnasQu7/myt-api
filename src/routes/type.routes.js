e27d12ab42eb91cdc6ea7e633ec326139968e11c
Mohd Anas Quraishi <105916661+AnasQu7@users.noreply.github.com>
2024-04-25 05:00:33 +0530

    Update type.routes.js

--- src/routes/type.routes.js
+++ src/routes/type.routes.js
@@ -9,6 +9,24 @@ typeRouter.post('/',async(req,res)=>{
        
     return res.status(201).send("type added")
 })
+typeRouter.get('/:id',async(req,res)=>{
+    const { id } = req.params;
+    
+    if(!id){
+    return res.status(404).send("Id not found");
+    }
+    
+   try{
+     let data = await typeModel.findById(id);
+    if (data) {
+      return res.status(201).send(data);
+    } else {
+      return res.status(404).send("not found");
+    }
+   }catch(err){
+       return res.status(404).send("not found");
+   }
+})
 typeRouter.get('/',async(req,res)=>{
     const body = req.body
     
